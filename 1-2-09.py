300e531408de9ad5e67b577de267a61e83c69cf3
LeeChinese <115471662+LeeChinese@users.noreply.github.com>
2023-03-17 16:53:13 +0800

    字符串的拼接

---
+++ 1-2-09.py
@@ -0,0 +1,9 @@
+# 字符串字面量之间的拼接
+print("人生苦短" + "我用python")
+print("人生苦短", "我用python")
+
+# 字符串字面量和字符串变量的拼接
+name = "程序员"
+address = "建材城东路9号院"
+tel = 400888
+print("我是:" + name + "，我的地址是：" + address + "，我的电话是：" + str(tel))
