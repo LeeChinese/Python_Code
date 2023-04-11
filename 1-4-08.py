667fe3d60c55ae94f0810c54f23365310897b97c
LeeChinese <115471662+LeeChinese@users.noreply.github.com>
2023-04-11 17:51:51 +0800

    range语句

--- 1-4-08.py
+++ 1-4-08.py
@@ -8,15 +8,35 @@ range语句：
 '''
 
 # range语法1 range（num）
-for x in range(10):
-    print(x)
+# for x in range(10):
+#     print(x)
+#
+# # range语法2 range（num1， num2）
+# for x in range(5, 10):
+#     # 从5开始，到10结束（不包含10本身）的一个数字序列，数字之间的间隔是1
+#     print(x)
+#
+# # range语法3 range（num1， num2，step）
+# for x in range(5, 10, 2):
+#     # 从5开始，到10结束（不包含10本身）的一个数字序列，数字之间的间隔是2
+#     print(x)
+# for x in range(10):
+#     print("送玫瑰花")
 
-# range语法2 range（num1， num2）
-for x in range(5, 10):
-    # 从5开始，到10结束（不包含10本身）的一个数字序列，数字之间的间隔是1
-    print(x)
 
-# range语法3 range（num1， num2，step）
-for x in range(5, 10, 2):
-    # 从5开始，到10结束（不包含10本身）的一个数字序列，数字之间的间隔是2
-    print(x)
+"""
+练习案例：
+有几个偶数
+"""
+
+# 通过键盘输入数字
+num = int(input("请输入数字序列的最大值"))
+# 定义变量记录偶数的数量
+count = 0
+
+# 通过for循环和if判断语句处理
+for x in range(1, num):
+    if x % 2 == 0:
+        count += 1
+
+print(count)
