97b4bae50fce102fb4003391ac1ff81b6ee1da1c
LeeChinese <115471662+LeeChinese@users.noreply.github.com>
2023-04-14 09:40:34 +0800

    列表的下标索引

---
+++ 1-6-03.py
@@ -0,0 +1,20 @@
+"""
+演示：列表的下标索引
+"""
+# 通过下标索引取出对应位置的数据
+my_list = ["Tom", "Lily", "Rose"]
+# 列表[下标索引]，从前往后从0开始，每次+1    从后往前从-1开始，每次-1
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+# 错误示范：通过下标索引引取数据，一定不要超出范围
+# print(my_list[3])
+
+# 通过下标索引取出数据(倒序取出)
+print(my_list[-1])
+print(my_list[-2])
+print(my_list[-3])
+
+# 取出嵌套列表的元素
+my_list = [[1, 2, 3], [4, 5, 6]]
+print(my_list[0][1])
